b4af73af6568f4fcbed3a715abb7230167e1d5f2
Harshaja Agnihotri <hn.agnihotri29@gmail.com>
2024-06-23 19:41:19 +0530

    added Nav, Menu and Dashboard components

---
+++ src/components/Menu.tsx
@@ -0,0 +1,16 @@
+
+const Menu = () => (
+    <nav className="col-md-2 d-none d-md-block bg-light sidebar">
+        <div className="sidebar-sticky">
+            <ul className="nav flex-column">
+                <li className="nav-item">
+                <a className="nav-link active" href="#">
+                    Dashboard
+                </a>
+                </li>
+            </ul>
+        </div>
+  </nav>
+)
+
+export default Menu
