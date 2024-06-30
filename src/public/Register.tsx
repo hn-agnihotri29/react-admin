3a2ad80d68dac8418f94d30cd24ee41113a606da
Harshaja Agnihotri <hn.agnihotri29@gmail.com>
2024-06-30 20:33:27 +0530

    added reidrect functionality on register page

--- src/public/Register.tsx
+++ src/public/Register.tsx
@@ -30,40 +30,43 @@ class Register extends Component {
     }
 
     render() {
-        if (this.state.redirect) {
-            return redirect("/login")
-        }
-
+        
         return (
-            <form className="form-signin" onSubmit={this.submit}>
-                <h1 className="h3 mb-3 font-weight-normal">Please register</h1>
-                <label htmlFor="firstName" className="sr-only">First Name</label>
-                <input type="text" id="firstName" className="form-control" placeholder="First Name" required 
-                    onChange={e => this.first_name = e.target.value}
-                />
+            <>
+                { this.state.redirect ? redirect("/login") : 
+                    <form className="form-signin" onSubmit={this.submit}>
+                        <h1 className="h3 mb-3 font-weight-normal">Please register</h1>
+                        <label htmlFor="firstName" className="sr-only">First Name</label>
+                        <input type="text" id="firstName" className="form-control" placeholder="First Name" required 
+                            onChange={e => this.first_name = e.target.value}
+                        />
 
-                <label htmlFor="lastName" className="sr-only">Last Name</label>
-                <input type="text" id="lastName" className="form-control" placeholder="Last Name" required
-                       onChange={e => this.last_name = e.target.value}
-                />
+                        <label htmlFor="lastName" className="sr-only">Last Name</label>
+                        <input type="text" id="lastName" className="form-control" placeholder="Last Name" required
+                            onChange={e => this.last_name = e.target.value}
+                        />
 
-                <label htmlFor="inputEmail" className="sr-only">Email address</label>
-                <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required
-                       onChange={e => this.email = e.target.value}
-                />
+                        <label htmlFor="inputEmail" className="sr-only">Email address</label>
+                        <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required
+                            onChange={e => this.email = e.target.value}
+                        />
 
-                <label htmlFor="inputPassword" className="sr-only">Password</label>
-                <input type="password" id="inputPassword" className="form-control" placeholder="Password"
-                       onChange={e => this.password = e.target.value}
-                       required/>
+                        <label htmlFor="inputPassword" className="sr-only">Password</label>
+                        <input type="password" id="inputPassword" className="form-control" placeholder="Password"
+                            onChange={e => this.password = e.target.value}
+                            required/>
 
-                <label htmlFor="passwordConfirm" className="sr-only">Password Confirm</label>
-                <input type="password" id="passwordConfirm" className="form-control" placeholder="Password Confirm"
-                       onChange={e => this.password_confirm = e.target.value}
-                       required/>   
-                <button className="btn btn-lg btn-primary btn-block" type="submit">Register</button>
-            </form>
-        );
+                        <label htmlFor="passwordConfirm" className="sr-only">Password Confirm</label>
+                        <input type="password" id="passwordConfirm" className="form-control" placeholder="Password Confirm"
+                            onChange={e => this.password_confirm = e.target.value}
+                            required/>   
+                        <button className="btn btn-lg btn-primary btn-block" type="submit">Register</button>
+                     </form>
+                    
+                }
+            </>
+        )
+            
     }
 }
 
