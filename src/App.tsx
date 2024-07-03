b71657a4e4ef127a945d6cae19fa7e5969e22459
Harshaja Agnihotri <hn.agnihotri29@gmail.com>
2024-07-03 20:20:28 +0530

    feat: added link and navlink

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Users from './secure/users/Users';
 import Login  from './public/Login';
 import Register from './public/Register';
+import RedirectToDashboard from './secure/RedirectToDashboard';
 
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
-            <Route path='/' element={<Dashboard />}/>
+            <Route path={'/'} index element={<RedirectToDashboard />}/>
+            <Route path='/dashboard' element={<Dashboard />}/>
             <Route path='/users' element={<Users />}/>
             <Route path='/login' element={<Login />} />
             <Route path={'/register'} element={<Register />}/>
--- src/secure/components/Menu.tsx
+++ src/secure/components/Menu.tsx
@@ -1,12 +1,18 @@
+import { NavLink } from "react-router-dom"
 
 const Menu = () => (
     <nav className="col-md-2 d-none d-md-block bg-light sidebar">
         <div className="sidebar-sticky">
             <ul className="nav flex-column">
                 <li className="nav-item">
-                <a className="nav-link active" href="#">
-                    Dashboard
-                </a>
+                    <NavLink to={'/dashboard'} className="nav-link">
+                        Dashboard
+                    </NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink to={'/users'} className="nav-link">
+                        Users
+                    </NavLink>
                 </li>
             </ul>
         </div>
