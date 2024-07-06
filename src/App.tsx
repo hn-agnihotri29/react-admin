6c2a0701f6546a65c2e1f65c18eb7a09c0c10556
Harshaja Agnihotri <hn.agnihotri29@gmail.com>
2024-07-06 20:17:02 +0530

    user edit page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Login  from './public/Login';
 import Register from './public/Register';
 import RedirectToDashboard from './secure/RedirectToDashboard';
 import UserCreate from './secure/users/UserCreate';
+import UserEdit from './secure/users/UserEdit';
 
 
 function App() {
@@ -16,9 +17,9 @@ function App() {
             <Route path='/dashboard' element={<Dashboard />}/>
             <Route path='/login' element={<Login />} />
             <Route path={'/register'} element={<Register />}/>
-            <Route path='/users' element={<Users />}/>
+            <Route path='/users/*' element={<Users />}/>
             <Route path='/users/create' element={<UserCreate />}/>
-
+            <Route path='/users/:id/edit' element={<UserEdit match/>}/>
         </Routes>
       </BrowserRouter>
   );
