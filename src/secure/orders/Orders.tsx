733c08200dbc384c60b276a999def3cd5f553f30
Harshaja Agnihotri <hn.agnihotri29@gmail.com>
2024-07-08 21:04:00 +0530

    [FIXTURE] Export the oders in csv file and download it

--- src/secure/orders/Orders.tsx
+++ src/secure/orders/Orders.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import {Component} from 'react';
 import Wrapper from "../Wrapper";
 import axios from 'axios';
@@ -28,10 +29,27 @@ class Orders extends Component {
 
         await this.componentDidMount();
     }
+    
+    handleExport = async () => {
+        const response = await axios.get('export', {responseType: 'blob'});
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const blob = new Blob([response.data], {type: 'text/csv'});
+        const downloadUrl = window.URL.createObjectURL(response.data);
+        const link = document.createElement('a');
+        link.href = downloadUrl;
+        link.download = 'orders.csv';
+        link.click();
+    }
 
     render() {
         return (
             <Wrapper>
+                <div
+                    className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                    <div className="btn-toolbar mb-2 mb-md-0">
+                        <a onClick={this.handleExport} className="btn btn-sm btn-outline-secondary">Export</a>
+                    </div>
+                </div>
                 <div className="table-responsive">
                     <table className="table table-striped table-sm">
                         <thead>
